533daac743ffac4f910f1be35ced6a143cdad3e0
Ashish Mishra <b.s.ashish26@gmail.com>
2017-08-05 16:40:59 +0530

    String to Number implemented

--- src/convert.js
+++ src/convert.js
@@ -1,55 +1,81 @@
-var convert={
-input:'two thousand ninty nine',
-digit:["one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"],
-hundred:["twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninty",],
-check:["one","two","three","four","five","six","seven","eight","nine","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"],
+var Convert = function(){
+this.input='125';
+this.one=["","one","two","three","four","five","six","seven","eight","nine"];
+this.digit=["ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"];
+this.tens=["","","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninty"];
+this.beyond=[" ","thousand","million","billion","Trillion","Quadrillion","Quintillion","Sextillion","Septillion","Octillion","Nonillion","Decillion","Undecillion","Duodecillion","Tredecillion","Quattuordecillion"];
 };
+var obj=new Convert();
 
-function toNum(input){
-    var len=input.length;
-    var sp=input.split(" ");
-    console.log(sp);
-    var num_digit=[];
-    for(var i=0;i<sp.length;i++)
-        {
-         if(convert.digit.indexOf(sp[i])>-1)
-            {
-         //console.log(sp[i]);
-         num_digit.push(convert.digit.indexOf(sp[i])+1);
-            }
-         if(convert.hundred.indexOf(sp[i])>-1)
-            {
-         //console.log(sp[i]);
-         num_digit.push((convert.hundred.indexOf(sp[i])+2)*10);
-            }
-        if(sp[i]==="hundred")
-            {
-         num_digit.push(100);
-            }
-        if(sp[i]==="thousand")
-            {
-         num_digit.push(1000);       
-            }  
-       
-          
-        }
-    var sum=0,number=1,multiply=1;
-    for(var j=num_digit.length-1;j>=0;j--)
-    {
-        number=1;
-        multiply=1;
-      if(convert.check.indexOf(num_digit[j])>-1)
-        {
-         number=num_digit[j];
-        }else{
-            multiply=num_digit[j];
-        }
-        
-        sum+=multiply*number;
-    } 
-     console.log(num_digit);
-     console.log(sum);
-    
+function numberToString(){
+   var number=document.getElementById("text_one").value;
+    document.getElementById("result").innerHTML=obj.number_to_string(number);
 }
+
+function stringToNumber(){
+   var string=document.getElementById("text_two").value;
+   document.getElementById("result").innerHTML=obj.string_to_number(string);
+
+}
+
+Convert.prototype.string_to_number = function(word){
+    var sp=word.split(" ");
+    console.log(sp);
+    var sum=0,multiply=1;
+    for(var j=sp.length-1;j>=0;)
+    {
+        if(this.one.indexOf(sp[j])>-1)
+        {
+          sum+=this.one.indexOf(sp[j])*multiply;
+          j--;
+          continue;
+        }
+        if(this.digit.indexOf(sp[j])>-1){
+          sum+=(this.digit.indexOf(sp[j])+10)*multiply;
+          j--;
+          continue;
+        }
+        if(this.tens.indexOf(sp[j])>-1)
+        {
+          sum+=((this.tens.indexOf(sp[j]))*10)*multiply;
+          j--;
+          continue;
+        }
+     if(sp[j]==="hundred"){
+           if(this.one.indexOf(sp[j-1])>-1){
+            sum+=100*this.one.indexOf(sp[j-1])*multiply;
+            j=j-2;
+        }
+        else{
+            sum+=100*multiply;
+            j--;
+        }
+        continue;
+     }
+     if(sp[j]==="thousand"){
+         multiply=1000;
+         j--;
+     }
+     if(sp[j]==="million"){
+         multiply=1000000;
+         j--;
+     }
+     if(sp[j]==="billion"){
+         multiply=1000000000;
+         j--;
+     }
+     if(sp[j]==="trillion"){
+         multiply=1000000000000;
+         j--;
+     }
+     if(sp[j]==="Quadrillion"){
+         multiply=1000000000000000;
+         j--;
+     }            
+       
+    }
+   return sum;
+    
+};
 
 
